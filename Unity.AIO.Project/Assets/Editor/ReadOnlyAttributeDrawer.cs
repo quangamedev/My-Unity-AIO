2fec903fe9728847a5c21aee2f4b5d785de80fb2
Quan Nguyen <quan.0512@hotmail.com>
2022-07-26 19:09:25 +0700

    Update ReadOnlyAttributeDrawer.cs
    Use Unity's way

--- Unity.AIO.Project/Assets/Editor/ReadOnlyAttributeDrawer.cs
+++ Unity.AIO.Project/Assets/Editor/ReadOnlyAttributeDrawer.cs
@@ -17,8 +17,9 @@ public class ReadOnlyAttributeDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label);
-        GUI.enabled = true;
+        using (new EditorGUI.DisabledScope(true))
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
     }
 }
