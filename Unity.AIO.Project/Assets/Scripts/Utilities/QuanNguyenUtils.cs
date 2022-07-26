ca46c886e221d94fb745fc9524028a1e9f0fb56e
Quan Nguyen <quan.0512@hotmail.com>
2022-07-26 19:09:44 +0700

    Update QuanNguyenUtils.cs
    Add Clear Editor Log method

--- Unity.AIO.Project/Assets/Scripts/Utilities/QuanNguyenUtils.cs
+++ Unity.AIO.Project/Assets/Scripts/Utilities/QuanNguyenUtils.cs
@@ -18,4 +18,12 @@ public static class QuanNguyenUtils
     {
         return layerMask == (layerMask | (1 << layer));
     }
+
+    public static void ClearEditorLogs()
+    {
+        var assembly = System.Reflection.Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method?.Invoke(new object(), null);
+    }
 }
